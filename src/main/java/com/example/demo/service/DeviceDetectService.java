9c3f22522127272e071c3d68b6841eab4cd69c7b
vxll <vell3.2@yandex.ru>
2025-04-02 19:26:57 +0500

    properties -> yaml & cmd highlight

--- src/main/java/com/example/demo/service/DeviceDetectService.java
+++ src/main/java/com/example/demo/service/DeviceDetectService.java
@@ -1,26 +1,26 @@
 package com.example.demo.service;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import java.util.logging.Logger;
 
 @Service
 public class DeviceDetectService {
     public String detect(String device) {
+        Logger logger = LoggerFactory.getLogger(DeviceDetectService.class);
         device = String.valueOf(device);
         System.out.println(device);
         if (device.contains("Mobile")) {
-            System.out.println("Moblie");
+            logger.info("Moblie");
             return "Mobile";
         } else if (device.contains("Windows")) {
-            System.out.println("Windows");
+            logger.info("Windows");
             return "Windows";
         } else if (device.contains("Mac")) {
-            System.out.println("Mac");
+            logger.info("Mac");
             return "Mac";
         }
-        System.out.println("Failed");
+        logger.warn("Failed");
         return "Failed";
     }
 }
